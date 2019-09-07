8f0ff420252a590d38fb94fdf7c58720250c62c6
Mitchell Budge <mitchellbudge@gmail.com>
2019-09-07 12:45:46 -0600

    Setup model object

---
+++ Pokedex/Pokedex/Model/Pokemon.swift
@@ -0,0 +1,13 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Mitchell Budge on 9/7/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon: Decodable {
+    let name: String
+}
